af3ac1490b61fe789570f8f9d73d9151a0f3817d
rfraposa <richraposa@gmail.com>
2022-07-21 07:09:35 -0700

    Removes the home page from /docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -652,6 +652,7 @@ const config = {
             {from: '/en/introduction/history',to: '/en/about-us/history'},
             {from: '/en/introduction/performance',to: '/en/about-us/performance'},
             {from: '/en/',to: '/en/intro'},
+            {from: '/',to: '/en/intro'},
             {from: '/en/getting-started/tutorial',to: '/en/tutorial'},
             {from: '/quick-start',to: '/en/quick-start'},
           ],
