7c777055368ad0815bee8a4ed151285c43b4f739
DanRoscigno <dan@roscigno.com>
2022-09-22 14:15:41 -0400

    fix ingest nav and WIP datasets nav

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -354,7 +354,7 @@ const config = {
             {from: '/en/introduction/possible_silly_questions',to: '/en/faq/general/'},
             {from: '/en/getting_started/',to: '/en/quick-start'},
             {from: '/en/guides/ingest/tab_separated_values',to: '/en/getting-started/example-datasets/nypd_complaint_data'},
-            {from: '/en/getting_started/example_datasets/',to: '/en/getting-started/example-datasets'},
+            {from: '/en/getting_started/example_datasets/',to: '/en/getting-started/example-datasets/'},
             {from: '/en/getting_started/example_datasets/amplab_benchmark',to: '/en/getting-started/example-datasets/amplab-benchmark'},
             {from: '/en/getting_started/example_datasets/criteo',to: '/en/getting-started/example-datasets/criteo'},
             {from: '/en/getting_started/example_datasets/metrica',to: '/en/getting-started/example-datasets/metrica'},
