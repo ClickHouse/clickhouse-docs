2031a506847fab9f04b914e2ffd1eb8856dca478
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-03-02 18:09:40 +0530

    Add sh content changes

--- copyClickhouseRepoDocs.sh
+++ copyClickhouseRepoDocs.sh
@@ -1,5 +1,8 @@
-#! /usr/bin/bash
+#! ./bin/bash
+echo "Start Cloning"
 git clone --branch new-nav https://github.com/ClickHouse/ClickHouse.git
+echo "Cloning completed"
+echo "Start Copying"
 cp -r ClickHouse/docs/en/development     docs/en/
 cp -r ClickHouse/docs/en/engines         docs/en/
 cp -r ClickHouse/docs/en/getting-started docs/en/
@@ -8,4 +11,8 @@ cp -r ClickHouse/docs/en/operations      docs/en/
 cp -r ClickHouse/docs/en/sql-reference   docs/en/
 cp -r ClickHouse/docs/ru                 docs/
 cp -r ClickHouse/docs/zh                 docs/
+echo "Copying completed"
+echo "Start Deleting ClickHouse"
 rm -r ClickHouse
+echo "Deleting ClickHouse folder completed"
+echo "----END----"
