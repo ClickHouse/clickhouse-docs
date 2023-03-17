174cfb86196b3e90d4502f6c8161e337bd0ff6ba
rfraposa <richraposa@gmail.com>
2023-03-16 22:42:56 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   onBrokenMarkdownLinks: 'throw',
   favicon: 'img/favicon.ico',
   organizationName: 'ClickHouse',
-  trailingSlash: true,
+  trailingSlash: false,
   projectName: 'clickhouse-docs',
   markdown: {
     mermaid: true,
