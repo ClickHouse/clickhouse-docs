f15c55a0ee1e6d8875bab965638d9342cd43a5d7
VINEETH ASOK KUMAR <me@vineethasok.com>
2023-05-08 14:33:53 +0200

    import fetch in header plugin

--- plugins/header.js
+++ plugins/header.js
@@ -2,6 +2,7 @@
 // `context` is provided by Docusaurus. Example: siteConfig can be accessed from context.
 // `opts` is the user-defined options.
 const menuItems = require('./menuItems.json')
+const fetch = require('node-fetch');
 async function chHeader(context, opts) {
   return {
     name: 'ch-header-plugin',
