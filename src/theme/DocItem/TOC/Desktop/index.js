69db0af6fe26d6f3e5ac8861d976ec9015cf4f24
VINEETH ASOK KUMAR <vineeth.asokkumar@clickhouse.com>
2023-05-25 12:21:48 +0100

    Add clickhouse cloud banner to TOC

---
+++ src/theme/DocItem/TOC/Desktop/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {ThemeClassNames} from '@docusaurus/theme-common';
+import {useDoc} from '@docusaurus/theme-common/internal';
+import TOC from '@theme/TOC';
+import clsx from "clsx";
+import styles from './styles.module.css'
+import IconClose from '@theme/Icon/Close';
+
+export default function DocItemTOCDesktop() {
+  const {toc, frontMatter} = useDoc();
+  return (
+    <div className={clsx(styles.docTOCContainer, 'theme-doc-toc-desktop-container')}>
+      <TOC
+        toc={toc}
+        minHeadingLevel={frontMatter.toc_min_heading_level}
+        maxHeadingLevel={frontMatter.toc_max_heading_level}
+        className={clsx(styles.docTOC, ThemeClassNames.docs.docTocDesktop)}
+      />
+      <div className={styles.docCloudCard}>
+        <div className={styles.docCloudCardHeader}>
+          <h6>Try ClickHouse Cloud for FREE</h6>
+          <button className={styles.docCloudClose}>
+            <IconClose color="var(--ifm-color-emphasis-600)" width={10} height={10}/>
+          </button>
+        </div>
+        <p className={styles.docCloudCardContent}>Easy data ingestion, automatic scaling, built-in SQL console and lots more.Try it for free</p>
+        <a href='https://clickhouse.cloud/signUp?loc=doc-card-banner' className={clsx(styles.docCloudCardLink, 'click-button primary-btn')}>Try it for Free</a>
+      </div>
+    </div>
+  );
+}
