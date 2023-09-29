c40bb2cdcdbbcc7bb23b5526dca833c8d4c399a0
justindeguzman <justin@justindeguzman.net>
2023-09-28 22:38:10 -0700

    Simplify storage and compute docs

--- sidebars.js
+++ sidebars.js
@@ -274,6 +274,7 @@ const sidebars = {
             },
             'en/architecture/single-node-deployment',
             'en/architecture/cluster-deployment',
+            'en/guides/separation_compute_storage'
           ],
         },
         {
