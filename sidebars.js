5eedcf3a6417a851bd295057ee56abec6bc3ce6b
justindeguzman <justin@justindeguzman.net>
2023-10-30 12:54:00 -0700

    Make beta and experimental features docs more concise

--- sidebars.js
+++ sidebars.js
@@ -318,6 +318,7 @@ const sidebars = {
             },
             'en/operations/configuration-files',
             'en/operations/server-configuration-parameters/settings',
+            'en/settings/beta-and-experimental-features'
           ],
         },
         {
