49bfd1fcdd91e231fe3952af35b104e6e9d4adf3
justindeguzman <justin@justindeguzman.net>
2024-07-02 20:46:27 -0700

    Add JSON redirect

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -297,6 +297,10 @@ const config = {
 			{
 				redirects: [
 					{
+						from: '/docs/en/integrations/data-ingestion/data-formats/json',
+						to: '/en/sql-reference/data-types/object-data-type'
+					},
+					{
 						from: '/en/sql-reference/data-types/json',
 						to: '/en/sql-reference/data-types/object-data-type'
 					},
