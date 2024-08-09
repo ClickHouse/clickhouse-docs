506c5174b8aa8c9c48e5225327c6e62b91c48d1d
Mark Needham <m.h.needham@gmail.com>
2024-08-09 13:13:01 +0100

    JupySQL/chDB guide

--- sidebars.js
+++ sidebars.js
@@ -1235,6 +1235,16 @@ const sidebars = {
     },
     {
       type: "category",
+      label: "Developer Guides",
+      className: "top-nav-item",
+      collapsed: false,
+      collapsible: false,
+      items: [
+        "en/chdb/guides/jupysql"
+      ],
+    },
+    {
+      type: "category",
       label: "About chDB",
       className: "top-nav-item",
       collapsed: false,
