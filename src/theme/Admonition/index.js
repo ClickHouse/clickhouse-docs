2622b8ca0cfef6e0d1be4d26ecd69220bf334fdd
Blargian <shaunstruwig.ss@gmail.com>
2024-11-13 19:22:53 +0100

    Fix style II

--- src/theme/Admonition/index.js
+++ src/theme/Admonition/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import clsx from 'clsx'
-import { ThemeClassNames } from '@docusaurus/theme-common'
-import Translate from '@docusaurus/Translate'
-import styles from './styles.module.css'
+import React from 'react';
+import clsx from 'clsx';
+import { ThemeClassNames } from '@docusaurus/theme-common';
+import Translate from '@docusaurus/Translate';
+import styles from './styles.module.css';
 
 function NoteIcon () {
   return (
@@ -12,9 +12,8 @@ function NoteIcon () {
         d="M6.3 5.69a.942.942 0 0 1-.28-.7c0-.28.09-.52.28-.7.19-.18.42-.28.7-.28.28 0 .52.09.7.28.18.19.28.42.28.7 0 .28-.09.52-.28.7a1 1 0 0 1-.7.3c-.28 0-.52-.11-.7-.3zM8 7.99c-.02-.25-.11-.48-.31-.69-.2-.19-.42-.3-.69-.31H6c-.27.02-.48.13-.69.31-.2.2-.3.44-.31.69h1v3c.02.27.11.5.31.69.2.2.42.31.69.31h1c.27 0 .48-.11.69-.31.2-.19.3-.42.31-.69H8V7.98v.01zM7 2.3c-3.14 0-5.7 2.54-5.7 5.68 0 3.14 2.56 5.7 5.7 5.7s5.7-2.55 5.7-5.7c0-3.15-2.56-5.69-5.7-5.69v.01zM7 .98c3.86 0 7 3.14 7 7s-3.14 7-7 7-7-3.12-7-7 3.14-7 7-7z"
       />
     </svg>
-  )
+  );
 }
-
 function TipIcon() {
   return (
     <svg viewBox="0 0 12 16">
@@ -25,7 +24,6 @@ function TipIcon() {
     </svg>
   );
 }
-
 function DangerIcon() {
   return (
     <svg viewBox="0 0 12 16">
@@ -36,7 +34,6 @@ function DangerIcon() {
     </svg>
   );
 }
-
 function InfoIcon() {
   return (
     <svg viewBox="0 0 14 16">
@@ -47,7 +44,6 @@ function InfoIcon() {
     </svg>
   );
 }
-
 function CautionIcon() {
   return (
     <svg viewBox="0 0 16 16">
@@ -58,7 +54,6 @@ function CautionIcon() {
     </svg>
   );
 }
-
 // Icon for custom :::experimental admonition
 function ExperimentIcon () {
   return (
@@ -70,7 +65,6 @@ function ExperimentIcon () {
     </svg>
   );
 }
-
 // Icon for custom :::obsolete admonition
 function ObsoleteIcon () {
   return (
