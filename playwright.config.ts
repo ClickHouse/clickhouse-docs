aace5a9f91268de380c8ab84e535c46b9eed1002
Dale Mcdiarmid <dale@clickhouse.com>
2025-01-08 20:04:37 +0000

    visual tests

---
+++ playwright.config.ts
@@ -0,0 +1,49 @@
+import {devices} from '@playwright/test';
+import type {PlaywrightTestConfig} from '@playwright/test';
+
+const isCI = !!process.env.CI; // Check if running in CI
+const baseURL = isCI ? process.env.BASE_URL : "http://localhost:3000";
+
+
+const config: PlaywrightTestConfig = {
+  fullyParallel: true,
+  webServer: {
+    port: 3000,
+    command: 'yarn docusaurus serve',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
+  reporter: [
+    // Use "dot" reporter on CI, "list" otherwise (Playwright default).
+    process.env.CI ? ["dot"] : ["list"],
+    // Add Argos reporter.
+    [
+      "@argos-ci/playwright/reporter",
+      {
+        // Upload to Argos on CI only.
+        uploadToArgos: isCI,
+
+        // Set your Argos token.
+        token: process.env.ARGOS_TOKEN,
+      },
+    ],
+  ],
+  timeout: 1200000, 
+  use: {
+    // On CI, we will set `BASE_URL` from Vercel preview URL
+    baseURL: baseURL,
+    extraHTTPHeaders: {
+        // Hide Vercel Toolbar in tests
+        "x-vercel-skip-toolbar": "0",
+    },
+  },
+
+};
+
+export default config;
---
+++ tests/utils.ts
@@ -0,0 +1,16 @@
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+
+export function extractSitemapPathnames(sitemap: string): string[] {
+  const $ = cheerio.load(sitemap, { xmlMode: true });
+  const urls: string[] = [];
+  $("loc").each(function handleLoc() {
+    urls.push($(this).text());
+  });
+  return urls.map((url) => new URL(url).pathname);
+}
+
+// Converts a pathname to a decent screenshot name
+export function pathnameToArgosName(pathname: string): string {
+  return pathname.replace(/^\/|\/$/g, "") || "index";
+}
