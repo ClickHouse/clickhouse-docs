31064bf1cc7a81f6e7c126abe75eb09132941e3c
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-01-23 18:37:42 +0100

    Various changes, experiments and updates

--- scripts/table-of-contents-generator/toc_gen.py
+++ scripts/table-of-contents-generator/toc_gen.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python3
+
 """
 This script can be used to automatically generate a table of contents (JSON file) from the markdown files in a directory,
 or multiple directories.
 """
 
-#!/usr/bin/env python3
-
 import json
 import os
 import argparse
 import sys
+from collections import defaultdict
+import yaml
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -21,42 +23,81 @@ def parse_args() -> argparse.Namespace:
         help="Generates a single TOC for all files in all sub-directories of provided directory. By default, generates TOC per folder.",
     )
     parser.add_argument(
+        "--out",
+        default=None,
+        help="Path to output the resulting table of contents file to (by default it is output to the provided directory - file is named according to --dir)"
+    )
+    parser.add_argument(
         "--dir",
         help="Path to a folder containing markdown (.md, .mdx) documents containing YAML with title, description, slug."
     )
+    parser.add_argument('--ignore', metavar='S', type=str, nargs='+',
+                        help='Directory names to ignore. E.g --ignore _snippets images')
     return parser.parse_args()
 
 def extract_title_description_slug(filename):
-    with open(filename, "r") as f:
-        lines = f.readlines()
+    data = defaultdict(str)
+    missing_fields = []
+    frontmatter_data = {}
 
-    title, description, slug = None, None, None
-    for line in lines:
-        if line.startswith("title:"):
-            title = line.strip().split(": ")[1]
-        if line.startswith("description:"):
-            description = line.strip().split(": ")[1]
-        elif line.startswith("slug:"):
-            slug = line.strip().split(": ")[1]
-    if title and slug and description:
-        return {"title": title, "description": description, "slug": slug, "dir": filename}
-    return None
+    try:
+        with open(filename, "r") as f:
+            content = f.read()
+            # find the first frontmatter tag
+            frontmatter_start = content.find('---\n')
+            if frontmatter_start != -1:
+                # find the second frontmatter tag
+                frontmatter_end = content.find('---\n', frontmatter_start + 4)
+                if frontmatter_start != -1:
+                    # find the second frontmatter tag
+                    frontmatter_end = content.find('---\n', frontmatter_start + 4)
+                    if frontmatter_end != -1:
+                        frontmatter_str = content[frontmatter_start+4:frontmatter_end]
+                        frontmatter_data = yaml.safe_load(frontmatter_str) or {}
 
-def walk_dirs(root_dir):
+        data.update(frontmatter_data)
+
+        if missing_fields:
+            print(f"Warning: {filename} is missing some fields:")
+            for field in missing_fields:
+                print(f"- {field}")
+
+        return frontmatter_data
+    except OSError as e:
+        print(f"Ran into a problem reading frontmatter: {e}")
+        sys.exit(1)
+def walk_dirs(root_dir, ignore_dirs=[]):
     for root, dirs, files in os.walk(root_dir):
+        # Modify the 'dirs' list in-place to remove ignored directories
+        dirs[:] = [d for d in dirs if d not in ignore_dirs
+                   and not any(d.startswith(ig) for ig in ignore_dirs)]
         yield root
 
-def write_to_file(json_array, output_path):
+def write_to_file(json_items, directory, output=None):
+
+    if output is not None:
+        # output to the given path the toc.json file
+        # If dir='docs/en/interfaces/formats' the file is called docs_en_interfaces_formats_toc.json
+        output_path = output+"/"+directory.replace("/", "_")
+    else:
+        output_path = directory
     try:
         os.makedirs(os.path.dirname(output_path), exist_ok=True)  # Create directories if they don't exist
         with open(output_path, "w") as f:
-            json.dump(json_array, f, indent=4)
+            json.dump(json_items, f, indent=4)
             f.write('\n')
+            print(f"Wrote {output_path}")
     except OSError as e:
         if e.errno == 21:
             print(f"Directory already exists: {e}")
         else:
             print(f"An error occurred creating directory: {e}")
+def write_file(json_items, args, directory):
+    print(args)
+    if args.out is not None:
+        write_to_file(json_items, directory+"/toc.json", args.out)
+    elif args.out is None:
+        write_to_file(json_items, directory+"/toc.json")
 
 def main():
 
@@ -66,13 +107,15 @@ def main():
     if root_dir is None:
         print("Please provide a directory with argument --dir")
         sys.exit(1)
-
-    if args.single_toc:
+    if os.path.lexists(root_dir) is False:
+        print("Path provided does not exist")
+        sys.exit(1)
+    if args.single_toc is True:
         json_items = [] # single list for all directories
 
-    for directory in walk_dirs(root_dir): # Walk directories
+    for directory in walk_dirs(root_dir, args.ignore): # Walk directories
 
-        if not args.single_toc:
+        if args.single_toc is False:
             json_items = [] # new list for each directory
 
         for filename in os.listdir(directory): # for each directory
@@ -85,19 +128,24 @@ def main():
                     result = extract_title_description_slug(full_path)
                     if result is not None:
                         json_items.append(result)
+                        if args.single_toc is False:
+                            # don't write toc.json for empty folders
+                            if len(json_items) != 0:
+                                json_items = sorted(json_items, key=lambda x: x.get("title"))
+                                # output to the specified directory if arg --out is provided
+                                write_file(json_items, args, directory)
+                            else:
+                                print("Ran into an issue trying to extract YAML: empty result")
 
-        if not args.single_toc:
-            json_array = sorted(json_items, key=lambda x: x.get("title"))
-
+        if args.single_toc is True:
             # don't write toc.json for empty folders
             if len(json_items) != 0:
-                write_to_file(json_items, directory+"/toc.json")
-
-    if args.single_toc:
-        json_array = sorted(json_items, key=lambda x: x.get("title"))
-        # don't write toc.json for empty folders
-        if len(json_items) != 0:
-            write_to_file(json_items, root_dir+"/toc.json")
+                json_array = sorted(json_items, key=lambda x: x.get("title"))
+                # output to the specified directory if arg --out is provided
+                write_file(json_items, args, directory)
+                sys.exit(0)
+            else:
+                sys.exit(1)
 
 if __name__ == "__main__":
     main()
