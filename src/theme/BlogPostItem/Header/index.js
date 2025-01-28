eb74f6058c52f21cf4c926c413787ab1f59c1178
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-01-28 11:56:40 +0100

    Update styling of KB page

---
+++ src/theme/BlogPostItem/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BlogPostItemHeaderTitle from '@theme/BlogPostItem/Header/Title';
+import BlogPostItemHeaderInfo from '@theme/BlogPostItem/Header/Info';
+import BlogPostItemHeaderAuthors from '@theme/BlogPostItem/Header/Authors';
+export default function BlogPostItemHeader() {
+  return (
+    <header>
+      <BlogPostItemHeaderTitle />
+      <BlogPostItemHeaderAuthors />
+    </header>
+  );
+}
