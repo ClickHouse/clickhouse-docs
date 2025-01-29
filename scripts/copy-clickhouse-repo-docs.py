53ea96ff0909eeada0a8a4d4d6986baf6e43933f
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-01-29 19:35:40 +0100

    correct parameters

--- scripts/copy-clickhouse-repo-docs.py
+++ scripts/copy-clickhouse-repo-docs.py
@@ -37,14 +37,14 @@ def copy_docs_locally(local_path):
     for folder in docs_folders_en:
       full_path = f"{local_path}/{folder}"
       #os.system(f"rsync -a {full_path} docs/en")
-      subprocess.run([rsync_path, "rsync", "-a", full_path, "docs/en"], check=True)
+      subprocess.run([rsync_path, "-a", full_path, "docs/en"], check=True)
     for folder in docs_folders_other:
       full_path = f"{local_path}/{folder}"
       #os.system(f"rsync -a {full_path} docs/")
-      subprocess.run([rsync_path, "rsync", "-a", full_path, "docs/"], check=True)
+      subprocess.run([rsync_path, "-a", full_path, "docs/"], check=True)
     for source_file in files_for_autogen_settings:
       #os.system(f"rsync -a ClickHouse/{source_file} scripts/tmp")
-      subprocess.run([rsync_path, "rsync", "-a", source_file, "scripts/tmp"], check=True)
+      subprocess.run([rsync_path, "-a", source_file, "scripts/tmp"], check=True)
   except Exception as e:
     print(e)
     sys.exit(1)
