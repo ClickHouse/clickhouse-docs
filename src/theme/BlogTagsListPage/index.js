51e3f08dcc7f2f771e82f154001e41ed43edb5ed
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-02-02 22:59:00 +0100

    Track swizzled BlogTagsListPage

---
+++ src/theme/BlogTagsListPage/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import clsx from 'clsx';
+import {
+  PageMetadata,
+  HtmlClassNameProvider,
+  ThemeClassNames,
+  translateTagsPageTitle,
+} from '@docusaurus/theme-common';
+import BlogLayout from '@theme/BlogLayout';
+import TagsListByLetter from '@theme/TagsListByLetter';
+import SearchMetadata from '@theme/SearchMetadata';
+import Heading from '@theme/Heading';
+import ButtonGroup from "../../components/ButtonGroup/ButtonGroup";
+import BlogBreadcrumbs from "../../components/BlogBreadcrumbs/BlogBreadcrumbs";
+export default function BlogTagsListPage({tags, sidebar}) {
+  const title = translateTagsPageTitle();
+  return (
+    <HtmlClassNameProvider
+      className={clsx(
+        ThemeClassNames.wrapper.blogPages,
+        ThemeClassNames.page.blogTagsListPage,
+      )}>
+      <PageMetadata title={title} />
+      <SearchMetadata tag="blog_tags_list" />
+      <BlogLayout sidebar={sidebar}>
+        <BlogBreadcrumbs/>
+        <Heading as="h1">{title}</Heading>
+        <ButtonGroup
+            onClick={function Da(value){value === 'recent' ? window.location.href = '/docs/knowledgebase' : window.location.href = '/docs/knowledgebase/tags' }}
+            options={[
+                {
+                    label: 'Recent',
+                    value: 'recent'
+                },
+                {
+                    label: 'Grouped by tags',
+                    value: 'grouped_by_tags'
+                },
+            ]}
+            selected="grouped_by_tags"
+            type="default"
+        />
+        <TagsListByLetter tags={tags} />
+      </BlogLayout>
+    </HtmlClassNameProvider>
+  );
+}
