86b3ca7032bd8fb8d6dd6ed197e741cd505a3321
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-02-17 23:05:15 +0100

    generate table of contents file for system settings

--- scripts/autogenerate-table-of-contents.sh
+++ scripts/autogenerate-table-of-contents.sh
@@ -17,6 +17,6 @@ pip install -r scripts/table-of-contents-generator/requirements.txt
 # Add runs of the script below for any table of contents files that need to be generated
 # You can run toc_gen.py --help for descriptions of the parameters
 python3 scripts/table-of-contents-generator/toc_gen.py --dir="knowledgebase" --single-toc --out="static" --ignore images
-
+python3 scripts/table-of-contents-generator/toc_gen.py --dir="docs/en/operations/system-tables" --single-toc --out="table-of-contents-files"
 deactivate
 rm -r venv
