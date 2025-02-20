aa1f4bb63430da9679a6e7e28454ee7249afd49a
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-02-20 11:44:14 +0100

    Only in ClickHouse Cloud -> Only has an effect in ClickHouse Cloud

--- scripts/settings/autogenerate-settings.sh
+++ scripts/settings/autogenerate-settings.sh
@@ -96,7 +96,7 @@ settings_from_cpp AS
 main_content AS
 (
     SELECT format('## {} {}\\n{}\\n{}\\n\\nType: {}\\n\\nDefault value: {}\\n\\n{}\\n\\n',
-                  name, '{#'||name||'}', multiIf(tier == 'Experimental', '<ExperimentalBadge/>', tier == 'Beta', '<BetaBadge/>', ''), if(description LIKE '%Only in ClickHouse Cloud%', '\\n<CloudAvailableBadge/>', ''), type, default, replaceOne(trim(BOTH '\\n' FROM description), ' and [MaterializedMySQL](../../engines/database-engines/materialized-mysql.md)',''))
+                  name, '{#'||name||'}', multiIf(tier == 'Experimental', '<ExperimentalBadge/>', tier == 'Beta', '<BetaBadge/>', ''), if(description LIKE '%Only has an effect in ClickHouse Cloud%', '\\n<CloudAvailableBadge/>', ''), type, default, replaceOne(trim(BOTH '\\n' FROM description), ' and [MaterializedMySQL](../../engines/database-engines/materialized-mysql.md)',''))
     FROM system.settings WHERE name IN settings_from_cpp
     ORDER BY name
 ),
