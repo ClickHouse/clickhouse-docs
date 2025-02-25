f41414e0ff86f431ca8b3c30a5796579f563b7bb
Paultagoras <paul.moore@clickhouse.com>
2025-02-25 02:40:35 -0500

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -624,8 +624,8 @@ const sidebars = {
               id: "integrations/language-clients/java/index"
             },
             // "integrations/language-clients/java/index",
-            "integrations/language-clients/java/client-v2",
-            "integrations/language-clients/java/jdbc-v2",
+            "integrations/language-clients/java/client",
+            "integrations/language-clients/java/jdbc",
             "integrations/language-clients/java/r2dbc"
           ]
         },
