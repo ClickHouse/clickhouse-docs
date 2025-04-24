162c9ae24e6c83c61d71f1c9485ce7a9aaafba83
Dale Mcdiarmid <dale@clickhouse.com>
2025-04-24 21:15:46 +0100

    feedback widget

--- src/theme/DocItem/TOC/Desktop/index.js
+++ src/theme/DocItem/TOC/Desktop/index.js
@@ -4,20 +4,10 @@ import {useDoc} from '@docusaurus/plugin-content-docs/client';
 import TOC from '@theme/TOC';
 import clsx from "clsx";
 import styles from './styles.module.css'
-import IconClose from '@theme/Icon/Close';
+import Feedback from '../../../../components/Feedback';
 
 export default function DocItemTOCDesktop() {
   const {toc, frontMatter} = useDoc();
-  const [isClosed, setClosed] = useState(true)
-  
-  useEffect(() => {
-    const closed =
-      window.localStorage.getItem('doc-cloud-card-banner') === 'closed'
-    if (!isClosed || closed !== isClosed) {
-      setClosed(closed)
-    }
-  }, [])
-  
   return (
     <div className={clsx(styles.docTOCContainer, 'theme-doc-toc-desktop-container')}>
       <TOC
@@ -26,14 +16,18 @@ export default function DocItemTOCDesktop() {
         maxHeadingLevel={frontMatter.toc_max_heading_level}
         className={clsx(styles.docTOC, ThemeClassNames.docs.docTocDesktop)}
       />
-      {!isClosed && (<div className={styles.docCloudCard}>
+      <div style={{'marginTop': toc.length < 7 ? '150px': '32px'}}>
+        <Feedback/>
+      </div>
+
+      <div className={styles.docCloudCard}>
         <div className={styles.docCloudCardHeader}>
           <h6>Try ClickHouse Cloud for FREE</h6>
         </div>
         <p className={styles.docCloudCardContent}>Separation of storage and compute, automatic scaling, built-in SQL console, and lots more. $300 in free credits when signing up.</p>
         <a href='https://console.clickhouse.cloud/signUp?loc=doc-card-banner'
            className={clsx(styles.docCloudCardLink, 'click-button primary-btn')}>Try it for Free</a>
-      </div>)}
+      </div>
     </div>
   );
 }
