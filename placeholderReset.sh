8877aebac8cbc6bdc74a7fbee5efdb68e1829bca
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-05-09 19:13:02 +0200

    lazy load block and show placeholder while doing so

--- placeholderReset.sh
+++ placeholderReset.sh
@@ -1,12 +1,7 @@
 #! ./bin/bash
-echo "Copying API placeholder files over generated content"
+echo "Copying placeholder files over generated content"
 PLACEHOLDER=docs/_placeholders 
 DOCS=docs
-cp $PLACEHOLDER/api/_invitations-api-reference.md   $DOCS/cloud/manage/api/invitations-api-reference.md
-cp $PLACEHOLDER/api/_keys-api-reference.md          $DOCS/cloud/manage/api/keys-api-reference.md
-cp $PLACEHOLDER/api/_members-api-reference.md       $DOCS/cloud/manage/api/members-api-reference.md
-cp $PLACEHOLDER/api/_organizations-api-reference.md $DOCS/cloud/manage/api/organizations-api-reference.md
-cp $PLACEHOLDER/api/_services-api-reference.md      $DOCS/cloud/manage/api/services-api-reference.md
 
 cp $PLACEHOLDER/changelog/_index.md                 $DOCS/whats-new/changelog/index.md
 echo "Copying completed"
--- src/theme/SettingsInfoBlock/SettingsInfoBlock.js
+++ src/theme/SettingsInfoBlock/SettingsInfoBlock.js
@@ -1,8 +1,22 @@
-import { Accordion, Table, Container } from '@clickhouse/click-ui/bundled'
 import styles from './styles.module.css';
+import React, { Suspense } from 'react'
+
+const Table = React.lazy(() => import('@clickhouse/click-ui/bundled').then(module => ({
+    default: module.Table
+})));
+
+function SettingsInfoBlockPlaceholder() {
+    // Create a placeholder with similar dimensions to the actual table
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.activity}></div>
+        </div>
+    );
+}
 
 const SettingsInfoBlock = ({type, default_value}) => {
     return(
+        <Suspense fallback={<SettingsInfoBlockPlaceholder />}>
         <div
             className={styles.settingsInfoBlock}
         >
@@ -30,6 +44,7 @@ const SettingsInfoBlock = ({type, default_value}) => {
                 className={styles.table}
             ></Table>
         </div>
+        </Suspense>
     )
 }
 
