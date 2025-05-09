037f4907e6bee45960bf71f2eb946a562fb397cb
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-05-09 20:26:15 +0200

    Use BrowserOnly

--- src/theme/SettingsInfoBlock/SettingsInfoBlock.js
+++ src/theme/SettingsInfoBlock/SettingsInfoBlock.js
@@ -1,5 +1,6 @@
 import styles from './styles.module.css';
-import React, { Suspense } from 'react'
+import React from 'react'
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const Table = React.lazy(() => import('@clickhouse/click-ui/bundled').then(module => ({
     default: module.Table
@@ -16,35 +17,37 @@ function SettingsInfoBlockPlaceholder() {
 
 const SettingsInfoBlock = ({type, default_value}) => {
     return(
-        <Suspense fallback={<SettingsInfoBlockPlaceholder />}>
-        <div
-            className={styles.settingsInfoBlock}
-        >
-            <Table
-                headers={[
-                    {
-                        label: 'Type'
-                    },
-                    {
-                        label: 'Default value'
-                    },
-                ]}
-                rows={
-                    [
-                        {
-                            id: "row-1",
-                            items: [
-                                {label: `${type}`},
-                                {label: `${default_value}`}
+        <BrowserOnly fallback={<SettingsInfoBlockPlaceholder />}>
+            {() => {
+                return <div
+                    className={styles.settingsInfoBlock}
+                >
+                    <Table
+                        headers={[
+                            {
+                                label: 'Type'
+                            },
+                            {
+                                label: 'Default value'
+                            },
+                        ]}
+                        rows={
+                            [
+                                {
+                                    id: "row-1",
+                                    items: [
+                                        {label: `${type}`},
+                                        {label: `${default_value}`}
+                                    ]
+                                }
                             ]
                         }
-                    ]
-                }
-                size="sm"
-                className={styles.table}
-            ></Table>
-        </div>
-        </Suspense>
+                        size="sm"
+                        className={styles.table}
+                    ></Table>
+                </div>
+            }}
+        </BrowserOnly>
     )
 }
 
