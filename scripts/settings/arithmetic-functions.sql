75aa1f06acf422b0ba1522c7d3367a55b6ce03dd
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-05-27 10:33:00 +0200

    change H3 to bold

--- scripts/settings/arithmetic-functions.sql
+++ scripts/settings/arithmetic-functions.sql
@@ -13,10 +13,10 @@ SELECT
         '{}{}{}{}{}{}',
         '## ' || name || ' ' || printf('{#%s}', name) || '\n\n',
         'Introduced in: v'||introduced_in||'\n\n',
-        '### Syntax {#syntax}\n\n'||printf('```sql\n%s\n```', syntax)||'\n\n',
-        '### Arguments {#arguments}\n\n'||arguments||'\n',
-        '### Returned value {#returned_value}\n\n'||trim(returned_value)||'\n\n',
-        '### Examples {#examples}\n\n'||examples||'\n'
+        '**Syntax**\n\n'||printf('```sql\n%s\n```', syntax)||'\n\n',
+        '**Arguments*\n\n'||arguments||'\n',
+        '**Returned value**\n\n'||trim(returned_value)||'\n\n',
+        '**Examples**\n\n'||examples||'\n'
     )
 FROM arithmetic_functions
 INTO OUTFILE 'arithmetic-functions.md' TRUNCATE FORMAT LineAsString
