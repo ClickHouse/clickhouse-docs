adfda87165c6920079b0fb171e267b0bea849fd3
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-05-27 10:42:37 +0200

    Update arithmetic-functions.sql

--- scripts/settings/arithmetic-functions.sql
+++ scripts/settings/arithmetic-functions.sql
@@ -14,7 +14,7 @@ SELECT
         '## ' || name || ' ' || printf('{#%s}', name) || '\n\n',
         'Introduced in: v'||introduced_in||'\n\n',
         '**Syntax**\n\n'||printf('```sql\n%s\n```', syntax)||'\n\n',
-        '**Arguments*\n\n'||arguments||'\n',
+        '**Arguments**\n\n'||arguments||'\n',
         '**Returned value**\n\n'||trim(returned_value)||'\n\n',
         '**Examples**\n\n'||examples||'\n'
     )
