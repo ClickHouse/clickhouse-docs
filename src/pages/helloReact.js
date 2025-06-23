f55ff87a824b880a6033f2faf1546b3a145d4fc2
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-06-23 17:21:36 +0200

    add page and basic components

---
+++ src/pages/helloReact.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import homepage_styles from './homepage_styles.module.scss'
+import {useColorMode} from "@docusaurus/theme-common";
+import Link from '@docusaurus/Link';
+import BookIcon from '@site/static/img/book.svg';
+import RocketIcon from '@site/static/img/rocket.svg';
+import LightbulbOnIcon from '@site/static/img/lightbulb-on.svg';
+import SearchBar from '@theme/SearchBar';
+
+const NavatticDemo = ({
+  demoId = "cmbj9y9dx000004lbbeus84ns",
+  width = "100%",
+  height = "600px",
+  className = ""
+}) => {
+    const src = `https://capture.navattic.com/${demoId}`;
+    return (
+        <div className={homepage_styles.navatticDemo} style={{ height }}>
+            <iframe
+                src={src}
+                style={{
+                    border: 'none',
+                    width: '100%',
+                    height: '100%'
+                }}
+                data-navattic-demo-id={demoId}
+                allow="fullscreen"
+                title="Navattic Interactive Demo"
+                loading="lazy"
+            />
+        </div>
+    );
+};
+
+const HeroSection = () => {
+    return(
+    <div className={homepage_styles.heroSection}>
+        <h2>The fastest and most resource efficient real-time data warehouse and open-source database.</h2>
+        <SearchBar/>
+    </div>
+    );
+}
+const NavatticDemoSection = () => {
+    return(
+        <div className={homepage_styles.navatticDemoSection}>
+            <NavatticDemo/>
+        </div>
+    )
+}
+
+const ExploreDocs = () => {
+    const { colorMode } = useColorMode();
+    return (
+        <div className={homepage_styles.exploreDocs}>
+            <div className={homepage_styles.panel}>
+                <div className={homepage_styles.panel_heading}>
+                    <RocketIcon/>
+                    <h2>Get started</h2>
+                </div>
+                <div className={homepage_styles.panel_links}>
+                    <Link>What is ClickHouse?</Link>
+                    <Link>Get started - Cloud</Link>
+                    <Link>Get started - Self-managed</Link>
+                    <Link>Install</Link>
+                    <Link>Migration guide</Link>
+                </div>
+            </div>
+            <div className={homepage_styles.panel}>
+                <div className={homepage_styles.panel_heading}>
+                    <LightbulbOnIcon/>
+                    <h2>Learn</h2>
+                </div>
+                <div className={homepage_styles.panel_links}>
+                    <Link>Concepts</Link>
+                    <Link>Best practices</Link>
+                    <Link>Use case guides</Link>
+                    <Link>Example datasets</Link>
+                </div>
+            </div>
+            <div className={homepage_styles.panel}>
+                <div className={homepage_styles.panel_heading}>
+                    <BookIcon/>
+                    <h2>Reference</h2>
+                </div>
+                <div className={homepage_styles.panel_links}>
+                    <Link>Syntax</Link>
+                    <Link>Settings</Link>
+                    <Link>Formats</Link>
+                    <Link>Engines</Link>
+                    <Link>Functions</Link>
+                    <Link>Data types</Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default function Hello() {
+    return (
+        <Layout title="Hello" description="Hello React Page">
+            <div className={homepage_styles.topLevelContainer}>
+                <div className={homepage_styles.yellowStrip}></div>
+                <HeroSection/>
+                <ExploreDocs/>
+                <NavatticDemoSection />
+            </div>
+        </Layout>
+    );
+}
