0f599085261ed44be1231443a904f0fd0897ef77
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-06-29 20:57:49 +0200

    add fix for layout shift on anchors

--- src/theme/DocRoot/Layout/index.js
+++ src/theme/DocRoot/Layout/index.js
@@ -5,7 +5,9 @@ import BackToTopButton from '@theme/BackToTopButton';
 import DocPageLayoutSidebar from '@theme/DocRoot/Layout/Sidebar';
 import DocPageLayoutMain from '@theme/DocRoot/Layout/Main';
 import styles from './styles.module.css';
+import { useAnchorFix } from '../../hooks/useAnchorFix';
 export default function DocPageLayout({children}) {
+  useAnchorFix(); // fix for layout shifts which occur when navigating to an anchor
   const sidebar = useDocsSidebar();
   const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
   return (
