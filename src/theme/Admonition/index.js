311b55eb7a694d214a55d5fe686e4150575a4272
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-07-08 22:05:40 +0200

    Update index.js

--- src/theme/Admonition/index.js
+++ src/theme/Admonition/index.js
@@ -172,7 +172,7 @@ const aliases = {
   secondary: 'note',
   important: 'info',
   success: 'tip',
-  warning: 'warning',
+  warning: 'caution',
 };
 function getAdmonitionConfig(unsafeType) {
   const type = aliases[unsafeType] ?? unsafeType;
