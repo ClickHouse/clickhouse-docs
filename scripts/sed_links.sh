75365590d077ba979be384c6147f72ac787a8f2e
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-07-17 13:10:24 +0200

    Update sed_links.sh

--- scripts/sed_links.sh
+++ scripts/sed_links.sh
@@ -11,14 +11,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
     sed -i '' 's|(../../quick-start\.mdx)|(/get-started/quick-start)|g' docs/operations/utilities/clickhouse-local.md
     sed -i '' 's/{#data-types-matching}/{#data-type-mapping}/g' docs/interfaces/formats/Avro/Avro.md
-    sed -i '' 's|(https://clickhouse.com/docs/sql-reference/statements/select#apply)|(/sql-reference/statements/select/)|g' docs/guides/developer/dynamic-column-selection.md
-    sed -i '' 's|(/sql-reference/statements/select#replace)|(/sql-reference/statements/select/)|g' docs/guides/developer/dynamic-column-selection.md
-    sed -i '' 's|(/sql-reference/statements/select#except)|(/sql-reference/statements/select/)|g' docs/guides/developer/dynamic-column-selection.md
+    sed -i '' 's|(https://clickhouse.com/docs/sql-reference/statements/select#apply)|(/sql-reference/statements/select)|g' docs/guides/developer/dynamic-column-selection.md
+    sed -i '' 's|(/sql-reference/statements/select#replace)|(/sql-reference/statements/select)|g' docs/guides/developer/dynamic-column-selection.md
+    sed -i '' 's|(/sql-reference/statements/select#except)|(/sql-reference/statements/select)|g' docs/guides/developer/dynamic-column-selection.md
 else
     # Linux
     sed -i 's|(../../quick-start\.mdx)|(/get-started/quick-start)|g' docs/operations/utilities/clickhouse-local.md
     sed -i 's/{#data-types-matching}/{#data-type-mapping}/g' docs/interfaces/formats/Avro/Avro.md
-    sed -i 's|(https://clickhouse.com/docs/sql-reference/statements/select#apply)|(/sql-reference/statements/select/)|g' docs/guides/developer/dynamic-column-selection.md
-    sed -i 's|(/sql-reference/statements/select#replace)|(/sql-reference/statements/select/)|g' docs/guides/developer/dynamic-column-selection.md
-    sed -i 's|(/sql-reference/statements/select#except)|(/sql-reference/statements/select/)|g' docs/guides/developer/dynamic-column-selection.md
+    sed -i 's|(https://clickhouse.com/docs/sql-reference/statements/select#apply)|(/sql-reference/statements/select)|g' docs/guides/developer/dynamic-column-selection.md
+    sed -i 's|(/sql-reference/statements/select#replace)|(/sql-reference/statements/select)|g' docs/guides/developer/dynamic-column-selection.md
+    sed -i 's|(/sql-reference/statements/select#except)|(/sql-reference/statements/select)|g' docs/guides/developer/dynamic-column-selection.md
 fi
