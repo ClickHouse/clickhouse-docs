0260b6ccaa3a431ce9462ff5cea1bcce92d3246d
Shaun Struwig <41984034+Blargian@users.noreply.github.com>
2025-07-20 20:21:10 +0200

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -764,7 +764,7 @@ const sidebars = {
           items: [
             "integrations/data-ingestion/clickpipes/mysql/index",
             "integrations/data-ingestion/clickpipes/mysql/faq",
-	    "integrations/clickpipes/mysql/schema-changes",
+	    "integrations/data-ingestion/clickpipes/mysql/schema-changes",
             {
               type: "category",
               label: "Source",
