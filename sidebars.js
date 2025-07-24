8ef702d4302c590d7b7e1268e3fbd3453ae73bc5
Amogh-Bharadwaj <amogh.bharadwaj@clickhouse.com>
2025-07-24 14:19:12 +0530

    add docs back to sidebar

--- sidebars.js
+++ sidebars.js
@@ -730,6 +730,8 @@ const sidebars = {
             "integrations/data-ingestion/clickpipes/postgres/deduplication",
             "integrations/data-ingestion/clickpipes/postgres/ordering_keys",
             "integrations/data-ingestion/clickpipes/postgres/toast",
+            "integrations/data-ingestion/dbms/postgresql/connecting-to-postgresql",
+            "integrations/data-ingestion/dbms/postgresql/inserting-data",
             "integrations/data-ingestion/clickpipes/postgres/schema-changes",
             "integrations/data-ingestion/clickpipes/postgres/faq",
             "integrations/data-ingestion/clickpipes/postgres/parallel_initial_load",
