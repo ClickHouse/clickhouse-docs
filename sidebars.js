2c5fa5a208b575042619ae95d2ddac5b4ea94974
wudidapaopao <xiaozhe.yu@clickhouse.com>
2026-03-26 03:58:12 +0800

    docs: add native python UDF documentation for chdb

--- sidebars.js
+++ sidebars.js
@@ -1706,6 +1706,7 @@ const sidebars = {
         'chdb/guides/querying-parquet',
         'chdb/guides/query-remote-clickhouse',
         'chdb/guides/clickhouse-local',
+        'chdb/guides/python-udf',
       ],
     },
     {
