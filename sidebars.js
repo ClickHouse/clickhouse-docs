9b060c147f9d62e555d62c9495420048fd959171
serprex <159546+serprex@users.noreply.github.com>
2026-07-02 16:18:32 +0000

    clickhouse-c: list under third party

--- sidebars.js
+++ sidebars.js
@@ -611,7 +611,6 @@ const sidebars = {
       collapsible: false,
       link: { type: 'doc', id: 'integrations/language-clients/index' },
       items: [
-        'integrations/language-clients/c',
         'integrations/language-clients/cpp',
         {
           type: 'category',
@@ -684,6 +683,7 @@ const sidebars = {
           collapsed: true,
           collapsible: true,
           items: [
+            'integrations/language-clients/c',
             'integrations/language-clients/moose-olap',
             'interfaces/third-party/client-libraries',
           ],
